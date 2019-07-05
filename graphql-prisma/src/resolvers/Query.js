bfa5b42f0251132f7c85f6f4107ee956c60c8bb5
Ice Lin <smalltides@gmail.com>
2019-07-05 18:40:53 +0800

    add pagination and sort feature

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -2,7 +2,12 @@ import getUserId from '../utils/getUserId'
 
 const Query = {
     users(parent, args, { prisma }, info) {
-        const opArgs = {}
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        }
         
         if (args.query) {
             opArgs.where = {
@@ -17,6 +22,10 @@ const Query = {
     myPosts(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 author: {
                     id: userId
@@ -36,6 +45,10 @@ const Query = {
     },
     posts(parent, args, { prisma }, info) {
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
@@ -52,7 +65,14 @@ const Query = {
         return prisma.query.posts(opArgs, info)
     },
     comments(parent, args, { prisma }, info) {
-        return prisma.query.comments(null, info)
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        }
+
+        return prisma.query.comments(opArgs, info)
     },
     me(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
